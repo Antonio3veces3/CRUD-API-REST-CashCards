b6b8d5f2d75d3e78b247fc0a407a94688eb36bac
Antonio3veces3 <cramirez28@ucol.mx>
2023-11-14 00:24:17 -0600

    :sparkles: Add paging and sorting to getAll cashCards

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -2,15 +2,15 @@ package com.example.cashcard;
 
 import java.lang.reflect.*;
 import java.net.URI;
-import java.security.Principal;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
-import org.apache.el.util.ReflectionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,14 +22,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.util.*;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -54,8 +48,17 @@ public class CashCardController {
 	}
 
 	@GetMapping("/all")
-	public ResponseEntity<Iterable<CashCard>> findAll() {
-		return ResponseEntity.ok(cashCardRepository.findAll());
+	public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+		Page<CashCard> page = cashCardRepository.findAll(
+			PageRequest.of(
+				pageable.getPageNumber(), 
+				pageable.getPageSize(), 
+				pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+				)
+		);
+
+		return ResponseEntity.ok(page.getContent());
+		//return ResponseEntity.ok(cashCardRepository.findAll());
 	}
 
 	@GetMapping("/{requestedId}")
@@ -69,11 +72,12 @@ public class CashCardController {
 	}
 
 	@PostMapping("")
-	private ResponseEntity<Void> AddNewCashCard(@RequestParam String username, @RequestParam Double amount, UriComponentsBuilder ucb) {
+	private ResponseEntity<Void> AddNewCashCard(@RequestParam String username, @RequestParam Double amount,
+			UriComponentsBuilder ucb) {
 		try {
-			//faltan validaciones de si ya existe ese ID
+			// faltan validaciones de si ya existe ese ID
 			CashCard newCashCard = new CashCard();
-			newCashCard.setId((int)(Math.random()*100000+1));
+			newCashCard.setId((int) (Math.random() * 100000 + 1));
 			newCashCard.setUsername(username);
 			newCashCard.setAmount(amount);
 			System.out.println("new ID: " + newCashCard.getId());
@@ -81,13 +85,14 @@ public class CashCardController {
 			URI locationOfNewCashCard = ucb.path("/cashcards/{id}").buildAndExpand(savedCashCard.getId()).toUri();
 			return ResponseEntity.created(locationOfNewCashCard).build();
 		} catch (Exception e) {
-			return ResponseEntity.badRequest().build(); 
+			return ResponseEntity.badRequest().build();
 		}
 
 	}
-	//PUT
+
+	// PUT
 	@PutMapping("/{requestedId}")
-	public ResponseEntity<Void> putCashCard(@PathVariable Integer requestedId, @RequestBody CashCard cashCardUpdate){
+	public ResponseEntity<Void> putCashCard(@PathVariable Integer requestedId, @RequestBody CashCard cashCardUpdate) {
 		// Faltan validaciones
 		Optional<CashCard> cashCard = cashCardRepository.findById(requestedId);
 		CashCard updatedCashCard = new CashCard();
@@ -98,29 +103,31 @@ public class CashCardController {
 		return ResponseEntity.noContent().build();
 	}
 
-	//PATCH 
+	// PATCH
 	@PatchMapping("/{requestedId}")
-	public ResponseEntity<CashCard> patchCashCard(@PathVariable Integer requestedId, @RequestBody Map<String, Object> fields){
+	public ResponseEntity<CashCard> patchCashCard(@PathVariable Integer requestedId,
+			@RequestBody Map<String, Object> fields) {
 		Optional<CashCard> cashCard = cashCardRepository.findById(requestedId);
-		if (cashCard.isPresent()){
-			fields.forEach((key, value)->{
+		if (cashCard.isPresent()) {
+			fields.forEach((key, value) -> {
 				Field field = ReflectionUtils.findField(CashCard.class, (String) key);
 				field.setAccessible(true);
 				ReflectionUtils.setField(field, cashCard.get(), value);
 			});
-				cashCardRepository.save(cashCard.get());
-				return ResponseEntity.ok(cashCard.get());
-			
-		}else {
+			cashCardRepository.save(cashCard.get());
+			return ResponseEntity.ok(cashCard.get());
+
+		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	//DELETE
+
+	// DELETE
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deleteCashCard(@PathVariable Integer id){
+	public ResponseEntity<Void> deleteCashCard(@PathVariable Integer id) {
 		cashCardRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
 
-	} 
+	}
 
 }
