949d4676d674a94fc7bdccac92ba952e804dc115
Antonio3veces3 <cramirez28@ucol.mx>
2023-11-15 00:27:04 -0600

    :white_check_mark: Add test to get cashcard by ID, and pass them

--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -4,15 +4,51 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+
+import java.util.Optional;
 
 @SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 	@Autowired 
 	TestRestTemplate restTemplate;
 
+	@Test
+	void shouldReturnNotFoundCashCardById() {
+		// select an id that does not exist
+		Integer id = 63;
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/" + id, String.class);
+		
+		
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+	
+	@Test 
+	void shouldReturnACashCardById(){
+		Integer id = 29012;
+		String uri = "/cashcards/" + id;
+		ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+		
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Integer userId = documentContext.read("$.id");
+		String username = documentContext.read("$.username");
+		Double amount = documentContext.read("$.amount");
+		
+		assertThat(userId).isEqualTo(29012);
+		assertThat(username).isEqualTo("Ana Ramirez");
+		assertThat(amount).isEqualTo(45687.00);
+		
+	}
+		
+	
+	
 	@Test
 	void shouldReturnACashCardWhenDataIsSaved() {
 		assertThat(1).isEqualTo(1);
